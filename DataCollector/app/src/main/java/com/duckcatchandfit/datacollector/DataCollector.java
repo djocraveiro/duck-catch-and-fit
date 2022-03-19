2680b851f0b161c092106b847723457858c3f588
David Craveiro <djocraveiro@gmail.com>
2022-03-19 17:23:20 +0000

    Create data collector project

---
+++ DataCollector/app/src/main/java/com/duckcatchandfit/datacollector/DataCollector.java
@@ -0,0 +1,22 @@
+package com.duckcatchandfit.datacollector;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class DataCollector implements SensorEventListener {
+
+    public DataCollector() {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
